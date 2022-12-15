66d75f022ba256471e1f77c9bb01b6f34e4cf16a
박해승 <2001phs92@gmail.com>
2022-12-15 18:51:27 +0900

    Composing Complex Interfaces

---
+++ Landmarks/Views/CategoryHome.swift
@@ -0,0 +1,39 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by 박해승 on 2022/12/15.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    @EnvironmentObject var modelData: ModelData
+    
+    var body: some View {
+        NavigationView {
+            List {
+                modelData.features[0].image
+                    .resizable()
+                    .scaledToFill()
+                    .frame(height: 200)
+                    .clipped()
+                    .listRowInsets(EdgeInsets())
+                
+                ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
+                    CategoryRow(categoryName: key, items: modelData.categories[key]!)
+                }
+                .listRowInsets(EdgeInsets())
+            }
+            .navigationTitle("Featured")
+            .listStyle(.plain)
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+            .environmentObject(ModelData())
+    }
+}
